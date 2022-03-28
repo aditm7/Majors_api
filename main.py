ee4269a7e5709f37aabd208b1e91e69ae30001ab
Adit Malhotra <aditmalhotra8@gmail.com>
2022-03-28 22:43:36 +0530

    Updating files

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import json
 import pandas as pds
-import math
 
 data = pds.read_excel("Data.xlsx")
 courses = {}
-slots = ["8:15-10:15","11:15-13:15","14:15-16:15","17:15-19:15","20:15-22:15"]
+slots = ["08:15-10:15","11:15-13:15","14:15-16:15","17:15-19:15","20:15-22:15"]
 
 for i in range (0,len(data),1):
   for s in slots:
