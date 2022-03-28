7ce9073b07b3dc4e9e578ff4966f9f1219762705
Adit Malhotra <aditmalhotra8@gmail.com>
2022-03-28 22:56:48 +0530

    Fixed bug: some not courses not showing

--- main.py
+++ main.py
@@ -9,12 +9,17 @@ for i in range (0,len(data),1):
   for s in slots:
     name = str(data[s].iloc[i])
     if len(name)!=3:
-      name=name[:6]
-      if name in courses:
-        courses[name]["LHCs"].append(data["Venue"].iloc[i])
-      else:
-          courses[name]={"Day":int(data["Date"].iloc[i]),"LHCs":[],"Time":s}
+      names=[]
+      names.append(name[:6])
+      if(len(name)>6):
+        if (name[6]=="/"): 
+          names.append(name[7:13])
+      for name in names:
+        if name in courses:
           courses[name]["LHCs"].append(data["Venue"].iloc[i])
+        else:
+            courses[name]={"Day":int(data["Date"].iloc[i]),"LHCs":[],"Time":s}
+            courses[name]["LHCs"].append(data["Venue"].iloc[i])
 
 with open("majors.json","w") as f:
     json.dump(courses, f)
